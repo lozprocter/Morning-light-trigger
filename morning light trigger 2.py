847065ac614b8bbf07f052f2633776f60e5af0f8
lozprocter <72624353+lozprocter@users.noreply.github.com>
2021-01-30 01:41:13 -1100

    Add files via upload

--- morning light trigger 2.py
+++ morning light trigger 2.py
@@ -5,12 +5,15 @@ import time
 #assigning gpio 17 to the relay
 relay = LED(17)
 
-#this section defines the funtion "flick_relay"
-def flick_relay():
+#this section defines the funtions to flick the relay on and off
+def relay_on():
   relay.on()
+def relay_off():
+  relay.off()
 
-#this schedules the flick_relay func to run at 07:00am every day
-schedule.every().day.at("07:00").do(flick_relay)
+#this schedules the light to come on at 07:00am until 09:00am every day
+schedule.every().day.at("07:00").do(relay_on)
+schedule.every().day.at("09:00").do(relay_off)
 
 #while True loop means that the code runs continuously
 #time.sleep function defines how long the light is kept on for
